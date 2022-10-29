862b543e1429572bb8cedad0fdca63aa5ee4feb2
Ke Chen <ke.samantha.chen@tum.de>
2022-10-29 22:13:49 +0200

    hw02 test

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -3,17 +3,25 @@
 
 uint64_t factorial(uint64_t val) {
     // TODO
-    return 0;
+    uint64_t result{1};
+    for (uint64_t i=val; i>0; i--) {
+        result*=i;
+    }
+    return result;
 }
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
     // TODO
-    return 0;
+    uint64_t denominator{factorial(val-val2)};
+    uint64_t numerator{factorial(val)};
+    return numerator/denominator;
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
     // TODO
-    return 0;
+    uint64_t denominator{factorial(val2)*factorial(val-val2)};
+    uint64_t numerator{factorial(val)};
+    return numerator/denominator;
 }
