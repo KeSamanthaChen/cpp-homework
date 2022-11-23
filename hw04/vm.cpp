7e79c91d7ffc357ec2f77c9d1ee292a3fda8124f
Ke Chen <ke.samantha.chen@tum.de>
2022-11-23 19:59:46 +0100

    submit hw04

--- hw04/vm.cpp
+++ hw04/vm.cpp
@@ -15,24 +15,146 @@ vm_state create_vm(bool debug) {
     // enable vm debugging
     state.debug = debug;
 
-
     register_instruction(state, "PRINT", [](vm_state& vmstate, const item_t /*arg*/) {
+        if (vmstate.stack.empty()) {
+            throw vm::vm_stackfail{"stack fail"};
+        }
         std::cout << vmstate.stack.top() << std::endl;
         return true;
     });
 
-
     // TODO create instructions
+    register_instruction(state, "LOAD_CONST", [](vm_state& vmstate, const item_t arg) {
+        vmstate.stack.push(arg);
+        return true;
+    });
+
+    register_instruction(state, "EXIT", [](vm_state& vmstate, const item_t /*arg*/) {
+        return false;
+    });
+
+    register_instruction(state, "POP", [](vm_state& vmstate, const item_t /*arg*/) {
+        if (vmstate.stack.empty()) {
+            throw vm::vm_stackfail{"stack fail"};
+        }
+        vmstate.stack.pop();
+        return true;
+    });
+
+    register_instruction(state, "ADD", [](vm_state& vmstate, const item_t /*arg*/) {
+        if (vmstate.stack.size() < 2) {
+            throw vm::vm_stackfail{"stack fail"};
+        }
+        item_t tos{vmstate.stack.top()};
+        vmstate.stack.pop();
+        item_t tos1{vmstate.stack.top()};
+        vmstate.stack.pop();
+        vmstate.stack.push(tos + tos1);
+        return true;
+    });
+
+    register_instruction(state, "DIV", [](vm_state& vmstate, const item_t /*arg*/){
+        if (vmstate.stack.size() < 2) {
+            throw vm::vm_stackfail{"stack fail"};
+        }
+        item_t tos{vmstate.stack.top()};
+        vmstate.stack.pop();
+        item_t tos1{vmstate.stack.top()};
+        vmstate.stack.pop();
+        if (tos == 0) {
+            throw vm::div_by_zero{"div by zero"};
+        }
+        vmstate.stack.push(tos1/tos);
+        return true;
+    });
+
+    register_instruction(state, "EQ", [](vm_state& vmstate, const item_t /*arg*/){
+        if (vmstate.stack.size() < 2) {
+            throw vm::vm_stackfail{"stack fail"};
+        }
+        item_t tos{vmstate.stack.top()};
+        vmstate.stack.pop();
+        item_t tos1{vmstate.stack.top()};
+        vmstate.stack.pop();
+        if (tos == tos1) {
+            vmstate.stack.push(1);
+        } else {
+            vmstate.stack.push(0);
+        }
+        return true;
+    });
+
+    register_instruction(state, "NEQ", [](vm_state& vmstate, const item_t /*arg*/){
+        if (vmstate.stack.size() < 2) {
+            throw vm::vm_stackfail{"stack fail"};
+        }
+        item_t tos{vmstate.stack.top()};
+        vmstate.stack.pop();
+        item_t tos1{vmstate.stack.top()};
+        vmstate.stack.pop();
+        if (tos == tos1) {
+            vmstate.stack.push(0);
+        } else {
+            vmstate.stack.push(1);
+        }
+        return true;
+    });
+
+    register_instruction(state, "DUP", [](vm_state& vmstate, const item_t /*arg*/){
+        if (vmstate.stack.empty()) {
+            throw vm::vm_stackfail{"stack fail"};
+        }
+        vmstate.stack.push(vmstate.stack.top());
+        return true;
+    });
+
+    register_instruction(state, "JMP", [](vm_state& vmstate, const item_t arg){
+        vmstate.pc = static_cast<size_t>(arg);
+        return true;
+    });
+
+    register_instruction(state, "JMPZ", [](vm_state& vmstate, const item_t arg){
+        if (vmstate.stack.empty()) {
+            throw vm::vm_stackfail{"stack fail"};
+        }
+        item_t tos{vmstate.stack.top()};
+        vmstate.stack.pop();
+        if (tos == 0) {
+            vmstate.pc = static_cast<size_t>(arg);
+        }
+        return true;
+    });
+
+    register_instruction(state, "WRITE", [](vm_state& vmstate, const item_t /*arg*/){
+        if (vmstate.stack.empty()) {
+            throw vm::vm_stackfail{"stack fail"};
+        }
+        vmstate.output_string.append(std::to_string(vmstate.stack.top()));
+        return true;
+    });
+
+    register_instruction(state, "WRITE_CHAR", [](vm_state& vmstate, const item_t /*arg*/){
+        if (vmstate.stack.empty()) {
+            throw vm::vm_stackfail{"stack fail"};
+        }
+        vmstate.output_string.push_back(static_cast<char>(vmstate.stack.top()));
+        return true;
+    });
 
     return state;
 }
 
 
 void register_instruction(vm_state& state, std::string_view name,
-                          const op_action_t& action) {
-    size_t op_id = state.next_op_id;
+                          const op_action_t& action) { // why the action is passed by reference? Too big?
+    size_t op_id = state.next_op_id; // instruction id
 
     // TODO make instruction available to vm
+    state.instruction_ids.emplace(name, op_id);
+    state.instruction_names.emplace(op_id, name);
+    state.instruction_actions.emplace(op_id, action);
+
+    state.next_op_id += 1;
 }
 
 
@@ -97,14 +219,31 @@ std::tuple<item_t, std::string> run(vm_state& vm, const code_t& code) {
             std::cout << "-- exec " << vm.instruction_names[op_id] << " arg=" << arg << " at pc=" << vm.pc << std::endl;
         }
 
+        if (vm.pc >= code.size()) {
+            throw vm::vm_segfault{"non-existent program address"};
+        }
+
         // increase the program counter here so its value can be overwritten
         // by the instruction when it executes!
         vm.pc += 1;
 
         // TODO execute instruction and stop if the action returns false.
+        auto instruaction = vm.instruction_actions[op_id];
+        if (instruaction(vm, arg) == false) {
+            // stop
+            if (vm.stack.empty()) {
+                throw vm::vm_stackfail{"stack fail"};
+            }
+            return {vm.stack.top(), vm.output_string};
+        }
     }
 
-    return {0, ""}; // TODO: return tuple(exit value, output text)
+    // return {0, ""}; // TODO: return tuple(exit value, output text)
+    if (vm.stack.empty()) {
+        throw vm::vm_stackfail{"stack fail"};
+    }
+    return {vm.stack.top(), vm.output_string}; // TODO: return tuple(exit value, output text)
+
 }
 
 
